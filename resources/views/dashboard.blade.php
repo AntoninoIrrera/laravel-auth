8e5f57016b6797401df1b06f4983114a4c77a377
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-02-20 14:09:25 +0100

    piccola prova inserire nome su dashbord

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,6 +18,8 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <p class="card-text">Benvenuto/a {{ Auth::user()->name }}</p>
                 </div>
             </div>
         </div>
