b52aac7fda453bef2f3b6f931dadd26d708cc5d8
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-02-21 21:42:47 +0100

    inserisco classe quando ce un errore nel form

--- resources/views/admin/shared/form.blade.php
+++ resources/views/admin/shared/form.blade.php
@@ -7,7 +7,7 @@
         <div class="mb-3">
 
             <label class="form-label" for="title">Inserisci il titolo</label>
-            <input class="form-control" type="text" value="{{old('title',$project->title)}}" name="title" id="title">
+            <input class="form-control {{$errors->has('title') ? 'is-invalid' : '' }}" type="text" value="{{old('title',$project->title)}}" name="title" id="title">
             @if($errors->has('title'))
             <div class="alert alert-danger mt-3">
                 @foreach ($errors->get('title') as $error)
@@ -25,7 +25,7 @@
         <div class="mb-3">
 
             <label class="form-label" for="relase_date">Inserisci la data</label>
-            <input class="form-control" type="date" value="{{old('relase_date',$project->relase_date)}}" name="relase_date" id="relase_date">
+            <input class="form-control {{$errors->has('relase_date') ? 'is-invalid' : '' }}" type="date" value="{{old('relase_date',$project->relase_date)}}" name="relase_date" id="relase_date">
             @if($errors->has('relase_date'))
             <div class="alert alert-danger mt-3">
                 @foreach ($errors->get('relase_date') as $error)
@@ -38,7 +38,7 @@
         <div class="mb-3">
 
             <label class="form-label" for="description">Inserisci la descrizione</label>
-            <textarea class="form-control" name="description" id="description" cols="30" rows="10">{{old('description',$project->description)}}</textarea>
+            <textarea class="form-control {{$errors->has('description') ? 'is-invalid' : ''}} " name="description" id="description" cols="30" rows="10">{{old('description',$project->description)}}</textarea>
             @if($errors->has('description'))
             <div class="alert alert-danger mt-3">
                 @foreach ($errors->get('description') as $error)
